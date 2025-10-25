6ddbc4a9d71d2786e292a4ae7b0ea2a161de8876
Shravankumar <shravankumarsayz@gmail.com>
2025-10-25 16:24:11 +0530

    RMX1901: Bring Voltage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/voltage_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-user \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    voltage_RMX1901-user \
+    voltage_RMX1901-userdebug \
+    voltage_RMX1901-eng
--- voltage_RMX1901.mk
+++ voltage_RMX1901.mk
@@ -6,11 +6,11 @@
 
 # Inherit some common Evolution stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_MINI_GAPPS := true
-TARGET_HAS_UDFPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_DISABLE_EPPE := true
 
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := voltage_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
