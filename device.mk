2b5f92be9389a8504d7e9313ae1c74d2243e1800
Shravankumar <shravankumarsayz@gmail.com>
2025-11-28 16:10:54 +0530

    RMX1901: Adapt to PixelOS
    Signed-off-by: Shravankumar <shravankumarsayz@gmail.com>

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-custom \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
